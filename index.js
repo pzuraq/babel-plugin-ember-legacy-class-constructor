e4b215888a645ab6bfc35ecde9cf62ade5dcdd2b
Christopher Garrett <me@pzuraq.com>
2017-09-30 00:13:31 -0700

    fixes bugs w/non-Ember classes, cleans up code

--- index.js
+++ index.js
@@ -6,76 +6,93 @@ module.exports = function (babel) {
     visitor: {
       Class(classPath) {
         for (let path of classPath.get('body.body')) {
+          // loop over the body to see if a constructor exists
           if (path.node.kind === 'constructor') {
             const { body } = path.node.body;
-            const maybeReturn = body[body.length - 1];
 
-            if (
-              maybeReturn
-              && maybeReturn.type === 'ReturnStatement'
-              && maybeReturn.argument.type === 'SequenceExpression'
-            ) {
-              body.pop();
+            path.traverse({
+              // iterate over the body and look for calls to `super`, convert them
+              // into calls to `super.init`. These can either be
 
-              const finalReturn = maybeReturn.argument.expressions.pop();
-              const expressions = maybeReturn.argument.expressions.map(e => t.expressionStatement(e));
-
-              body.push(...expressions, t.returnStatement(finalReturn));
-            }
-
-            for (let i = 0; i < body.length; i++) {
-              const node = body[i];
-
-              if (node.type !== 'ExpressionStatement') continue;
-
-              const { expression } = node;
-
-              if (
-                expression.type === 'CallExpression'
-                && expression.callee.type === 'Super'
-              ) {
-                body[i] = t.expressionStatement(
-                  t.callExpression(
-                    t.memberExpression(t.super(), t.identifier('init')),
-                    [
-                      t.spreadElement(
-                        t.identifier('arguments')
-                      )
-                    ]
-                  )
-                );
-
-                break;
-              } else if (
-                expression.type === 'AssignmentExpression'
-                && expression.right.type === 'CallExpression'
-                && expression.right.callee.type === 'Super'
-              ) {
-                body[i] = t.expressionStatement(
-                  t.assignmentExpression(
-                    '=',
-                    expression.left,
+              CallExpression(path) {
+                if (path.node.callee.type === 'Super') {
+                  path.replaceWith(
                     t.callExpression(
                       t.memberExpression(t.super(), t.identifier('init')),
-                      [
-                        t.spreadElement(
-                          t.identifier('arguments')
-                        )
-                      ]
+                      path.node.arguments
                     )
-                  )
-                );
+                  );
+                }
+              },
 
-                break;
+              // Sequences don't transpile correctly most of the time, if
+              // one exists as the return statement pop it off and turn it into
+              // a series of expressions (usually it's the result of transpiling)
+              ReturnStatement(path) {
+                if (path.node.argument.type === 'SequenceExpression') {
+                  const expressions = path.node.argument.expressions;
+
+                  const finalReturn = expressions.pop();
+                  const expressionStatements = expressions.map(e => t.expressionStatement(e));
+
+                  path.replaceWithMultiple(
+                    ...expressionStatements,
+                    finalReturn
+                  );
+                }
               }
+            });
+
+            // Add a constructor to call init for non-Ember Object classes
+            const constructorBody = [
+              t.returnStatement(
+                t.callExpression(
+                  t.memberExpression(
+                    t.thisExpression(),
+                    t.identifier('init')
+                  ),
+                  [
+                    t.spreadElement(
+                      t.identifier('arguments')
+                    )
+                  ]
+                )
+              )
+            ];
+
+            // If the class has a super class then we need to call super before
+            // calling init. Call it with the value `EMBER_LEGACY_SHORT_CIRCUIT`
+            // to prevent it from calling init prematurely
+            if (classPath.node.superClass !== null) {
+              constructorBody.unshift(
+                t.expressionStatement(
+                  t.callExpression(
+                    t.super(),
+                    [t.stringLiteral('EMBER_LEGACY_SHORT_CIRCUIT')]
+                  )
+                )
+              );
             }
 
-            path.replaceWith(
-              t.classMethod('method', t.identifier('init'), [], t.blockStatement(body))
-            )
-
-            path.__emberLegacyConverted = true;
-            break;
+            path.replaceWithMultiple([
+              t.classMethod(
+                'method',
+                t.identifier('init'),
+                path.node.params,
+                t.blockStatement(body)
+              ),
+              t.classMethod(
+                'constructor',
+                t.identifier('constructor'),
+                [t.identifier('shortCircuit')],
+                t.blockStatement([
+                  t.ifStatement(
+                    t.binaryExpression('!==', t.identifier('shortCircuit'), t.stringLiteral('EMBER_LEGACY_SHORT_CIRCUIT')),
+                    t.blockStatement(constructorBody)
+                  )
+                ])
+              )
+            ]);
           }
         }
       }
